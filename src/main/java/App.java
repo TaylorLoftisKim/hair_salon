fa900ef9101adc8f12315e6eab9d4cf60ac638b8
Taylor Loftis-Kim <taylorloftiskim@gmail.com>
2017-04-12 15:52:25 -0700

    Rebuild vtl pages.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -27,7 +27,7 @@ public class App {
       String detail = request.queryParams("detail");
       Stylist newStylist = new Stylist(name, detail);
       newStylist.save();
-      model.put("template", "templates/success.vtl");
+      model.put("template", "templates/success-stylist.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
@@ -35,6 +35,40 @@ public class App {
       Map<String, Object> model = new HashMap<String, Object>();
       model.put("stylists", Stylist.all());
       model.put("template", "templates/stylists.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/stylists/:id", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      Stylist stylist = Stylist.find(Integer.parseInt(request.params(":id")));
+      model.put("stylist", stylist);
+      model.put("template", "templates/stylist.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/stylists/$stylist.getStylistId()", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      Client client = Client.find(Integer.parseInt(request.params("id")));
+      Stylist stylist = Stylist.find(client.getStylistId());
+      client.delete();
+      model.put("stylist", stylist);
+      model.put("template", "templates/stylist.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/client/new", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/client-form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/client", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      String name = request.queryParams("name");
+      String detail = request.queryParams("detail");
+      Client newClient = new Client(name, detail);
+      newClient.save();
+      model.put("template", "templates/success-client.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
